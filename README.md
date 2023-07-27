2ae48ba0471006b75615d96407d2487cb7f75873
Sammy Baek <whitesw01@gmail.com>
2023-07-27 23:45:12 +0900

    Init general settings & envs

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native Tutorial - Diary
+
+노마드 코더스 React Native 마스터클래스 <https://nomadcoders.co/react-native-masterclass>
+
+Third App.
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components/native';
+
+const defaultTheme: DefaultTheme = {
+  mode: 'light',
+};
+
+export default defaultTheme;
